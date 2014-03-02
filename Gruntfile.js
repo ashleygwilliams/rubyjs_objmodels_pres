698fe2bc2dfe236bf80a4a38b444288ef135fdda
ashley williams <heyashleyashley@gmail.com>
2014-03-02 16:23:50 -0500

    build ashley theme and scaffold presentation
    - add ashley.scss
    - add futura oblique and koyu fonts
    - update grunt file
    - create custom.css
    - include require files in index.html
    - scaffold basic slides, headings, and transitions in index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,9 @@ module.exports = function(grunt) {
 					'css/theme/sky.css': 'css/theme/source/sky.scss',
 					'css/theme/moon.css': 'css/theme/source/moon.scss',
 					'css/theme/solarized.css': 'css/theme/source/solarized.scss',
-					'css/theme/blood.css': 'css/theme/source/blood.scss'
+					'css/theme/blood.css': 'css/theme/source/blood.scss',
+					'css/theme/ashley.css': 'css/theme/source/ashley.scss',
+					'css/custom.css': 'css/custom.scss'
 				}
 			}
 		},
